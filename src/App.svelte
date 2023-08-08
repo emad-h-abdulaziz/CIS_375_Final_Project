9486ec4ddb38346de6fa91d3b96f24052f748061
emad-h-abdulaziz <emada3691@gmail.com>
2023-08-08 00:05:44 -0400

    Initial files from Sumna

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+    // Function for the Start button
+    function startFunction() {
+        // Add your implementation here
+        console.log("Start button clicked");
+    }
+
+    // Function for the Stop button
+    function stopFunction() {
+        // Add your implementation here
+        console.log("Stop button clicked");
+    }
+
+    // Function to handle the algorithm selection
+    function handleAlgorithmSelection(event) {
+        const selectedAlgorithm = event.target.value;
+        // Add your implementation here based on the selected algorithm
+        console.log("Selected Algorithm:", selectedAlgorithm);
+    }
+</script>
+
+<style>
+    /* Your CSS styles go here */
+    body {
+        font-family: Arial, sans-serif;
+        text-align: center;
+        margin: 20px;
+    }
+
+    button {
+        margin: 5px;
+        padding: 10px 20px;
+        font-size: 16px;
+    }
+
+    label {
+        font-size: 16px;
+    }
+
+    select {
+        font-size: 16px;
+        padding: 5px;
+    }
+</style>
+
+<h1>Welcome to the Website Prototype</h1>
+<button on:click={startFunction}>Start</button>
+<button on:click={stopFunction}>Stop</button>
+<label for="algorithmSelect">Choose an Algorithm:</label>
+<select id="algorithmSelect" on:change={handleAlgorithmSelection}>
+    <option value="bubble">Bubble Sort</option>
+    <option value="merge">Merge Sort</option>
+    <option value="heap">Heap Sort</option>
+    <!-- Add more algorithms as needed -->
+</select>
